64864c07b6b8644b2e7d120988d71997438c777d
KornPattamakorn <pattamakorn.s@hotmail.com>
2019-09-30 02:39:58 +0700

    Student Attendent

--- app/src/main/java/com/example/studentattendent/detailstudent.java
+++ app/src/main/java/com/example/studentattendent/detailstudent.java
@@ -2,8 +2,12 @@ package com.example.studentattendent;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -35,36 +39,38 @@ import java.util.Map;
 public class detailstudent extends AppCompatDialogFragment {
     View v;
     private EditText edtextpost;
-    private TextView detailST;
+    private TextView detailST,dtels,classd,parentd,adviserd,telAdvi;
     private detailstudent.detailListener listener;
     private ImageView imgProfile;
-    private String textPost;
-    private String URL_postnews = "http://203.154.83.137/StudentAttendent/dtailst.php";
+    private String URL_postdetail = "http://203.154.83.137/StudentAttendent/dtailst.php";
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(getActivity(),R.style.AlertDialogCustom));
+        //AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.profile,null);
-        builder.setView(view)
-                .setTitle(textPost)
-                .setNegativeButton("", new DialogInterface.OnClickListener() {
+        v = inflater.inflate(R.layout.profile,null);
+
+        builder.setView(v)
+                .setTitle("ข้อมูลส่วนตัว")
+                .setPositiveButton("ปิด", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
 
                     }
-                })
-                .setPositiveButton("ซ่อน", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        detailST.setText("Korn");
-                    }
                 });
-        //edtextpost = view.findViewById(R.id.postedittext);
-        detailST = view.findViewById(R.id.dname);
-        imgProfile = view.findViewById(R.id.detailProfile);
+
+        detailST = v.findViewById(R.id.dname);
+        dtels = v.findViewById(R.id.dtel);
+        classd = v.findViewById(R.id.dclass);
+        parentd = v.findViewById(R.id.dparent);
+        adviserd = v.findViewById(R.id.dadviser);
+        telAdvi = v.findViewById(R.id.dTadviser);
+        imgProfile = v.findViewById(R.id.detailProfile);
+        loadDetail();
+
         return builder.create();
     }
     public interface  detailListener{
@@ -74,7 +80,7 @@ public class detailstudent extends AppCompatDialogFragment {
 
     public void loadDetail(){
         StringRequest stringRequest = new StringRequest(Request.Method.POST,
-                URL_postnews, new Response.Listener<String>() {
+                URL_postdetail, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 Log.d("ResponseStudent",response.toString());
@@ -82,13 +88,30 @@ public class detailstudent extends AppCompatDialogFragment {
                     JSONArray array = new JSONArray(response);
                     for (int i = 0; i < array.length(); i++) {
                         JSONObject posts = array.getJSONObject(i);
-                        String imgPro = posts.getString("imgst");
-                        Glide.with(v.getContext()).load(imgPro).into(imgProfile);
+                        String fsname = posts.getString("fnamest");
+                        String lsname = posts.getString("lnamest");
+                        String classS = posts.getString("class");
+                        String fpname = posts.getString("fnamePr");
+                        String lpname = posts.getString("lnamePr");
+                        String tpname = posts.getString("parenttel");
+                        String tsname = posts.getString("stel");
+                        String fnamet = posts.getString("fnameTC");
+                        String lnamet = posts.getString("lnameTC");
+                        String ttel = posts.getString("ttel");
+                        String Tpic = posts.getString("imgst");
+
+                        detailST.setText("● ชื่อ-นามสกุล: \n"+"    " +fsname+" "+lsname);
+                        dtels.setText("● เบอร์โทรศัพท์: \n"+"    " +tsname);
+                        classd.setText("● ชั้นมัธยมศึกษาปีที่ "+classS);
+                        parentd.setText("● ผู้ปกครอง: \n"+"    " +fpname+" " +lpname+"\n\n" +
+                                "● เบอร์โทรศัพท์ผู้ปกครอง:\n    "+tpname);
+                        adviserd.setText("● ครูประจำชั้น: \n"+"    "+fnamet+" "+lnamet);
+                        telAdvi.setText("● เบอร์โทรศัพท์ครูประจำชั้น: \n"+"    "+ttel);
+                        Glide.with(v.getContext()).load(Tpic).into(imgProfile);
 
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
-                    //Toast.makeText(getActivity(),e.toString(), Toast.LENGTH_SHORT).show();
                 }
 
             }
@@ -97,19 +120,16 @@ public class detailstudent extends AppCompatDialogFragment {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Log.d("abc",error.toString());
-                        // Toast.makeText(getActivity(),error.toString(), Toast.LENGTH_SHORT).show();
                     }
 
                 }) {
-//
-//            @Override
-//            protected Map<String, String> getParams() throws AuthFailureError {
-//                Map<String,String> params = new HashMap<>();
-//                params.put("nameteachpost","teacher");
-//                params.put("topic","admins");
-//                params.put("pnews",textPost);
-//                return params;
-//            }
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("user","admins");
+                return params;
+            }
 
         };
         RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
